f7ed927eed3c3e3677262060efae56ca7d7ff1e8
DESKTOP-TFJU959\Candido <cleydson21_123_@hotmail.com>
2017-06-11 15:14:57 -0300

    Att

--- src/negocios/NegocioCliente.java
+++ src/negocios/NegocioCliente.java
@@ -28,20 +28,29 @@ public class NegocioCliente {
         if (cliente.getNome().length() > 100) {
             throw new Exception("Insira no máximo 100 caracteres!");
         }
-        
         if (cliente.getNome().matches("[a-zA-Z_\\s]+") == false) {
             throw new Exception("O nome deve ser preenchido apenas com letras");
         }
+        
+        
         if (cliente.getBairro() == null) {
             throw new Exception("Informar o bairro!");  
         }
         if (cliente.getBairro().trim().equals("")==true) {
             throw new Exception("informar o bairro!"); 
         }
-        if (cliente.getBairro().length()< 3) {
-            throw new Exception("");
-            
+        if (cliente.getBairro().length()< 4) {
+            throw new Exception("Bairro muito curto");     
         }
+        if (cliente.getBairro().length() > 50) {
+            throw new Exception("Insira no máximo 50 caracteres!");
+        }
+        if (cliente.getBairro().matches("[a-zA-Z_\\s]+") == false) {
+            throw new Exception("O bairro deve ser preenchido apenas com letras");
+        }
+        
+        
+        
         DadosCliente dados = new DadosCliente();
         dados.cadastrar(cliente);
     }
